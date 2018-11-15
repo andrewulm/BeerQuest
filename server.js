7c6beb32439e487e17a816a247a364193fb697eb
Byron Ferguson <ferguson.bs@gmail.com>
2018-11-15 00:17:14 -0600

    re-adding mongo

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,10 @@ server.use(express.static('public'));
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(routes);
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/bookmarks-fyi";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Listen on the port
 server.listen(PORT, function() {
