ec2181e9e978258f8351a7728022b3b0ce04a92d
unknown <whigs93@gmail.com>
2018-12-02 00:19:05 -0600

    db no seeds

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,11 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS beerquest_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE DATABASE brewmaster_db;
+
+CREATE TABLE reviews (
+review_id INT NOT NULL AUTO_INCREMENT,
+review_username VARCHAR(255) NULL, 
+score DECIMAL(5,1) NULL,
+review VARCHAR(280) NULL,
+PRIMARY KEY (review_id)
+);
