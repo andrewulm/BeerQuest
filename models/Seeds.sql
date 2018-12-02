b8df533625b060f2cb6ddb941a70698f9e730372
unknown <whigs93@gmail.com>
2018-12-02 00:42:43 -0600

    seeds

---
+++ models/Seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Anti-Burg", false);
+
+USE brewmaster_db;
+
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Boulevard Wheat", "great with a lemon!",5);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Bud Light", "It's ok!",3);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Coors Light", "Crack open a cold with the boys, IT's COORS TIME",4);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Shock Top", "tastes like an orange spit in a bottle :(",3);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Root Beer", "TOTALLY SUCKS AINT EVEN GET ME DRUNK",1);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Guinness", "love a cold one with me bros and watch the big game",5);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Corona", "enjoy one on the beach! no really try it!",4);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("PABST Blue Ribbon", "I mean, It's cheap!",2);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Rolling Rock", "I love the nice green glass! bottle in the vats of old latrobe!",4.5);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Boulevard Pale Ale", "I don't really enjoy this one, but it's still good",3);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Mike's Hard Lemonade", "Way too sugary",1);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Sam Adams Boston Lager", "Sam Adams founded a country in my mouth... OF FLAVOR",4);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Red Stripe Lager", "It's good, but do the bottles make it expensive?",3);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Modelo Especial", "this is just not good at all",1);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Ad Adstra Ale" "Had it a few times! Free State always good",5);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Stella Artois", "I feel superior when im gripping and sipping that Stella",5);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Budweiser", "standard fare",2);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Sapporo Original", "Had this at the sushi place! LOVED IT",3);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Yuengling Original Lager", "gold is good!",3);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Kirin Ichiban", "very pale like rolling rock squared",2);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Boulevard Hazy IPA", "love the limtied editions",4);
+INSERT INTO beer (beer_name, beer_desc, beer_rating) VALUES ("Boulevard NutCracker", "a nice warming brew",5);
