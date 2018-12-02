73c341fb75b12f4e2e37ede509a95e64249e23a9
unknown <whigs93@gmail.com>
2018-12-02 00:24:28 -0600

    beer table

--- models/schema.sql
+++ models/schema.sql
@@ -11,3 +11,11 @@ score DECIMAL(5,1) NULL,
 review VARCHAR(280) NULL,
 PRIMARY KEY (review_id)
 );
+
+CREATE TABLE beer (
+beer_id INT NOT NULL AUTO_INCREMENT,
+beer_name VARCHAR(255) NOT NULL,
+beer_desc VARCHAR(255),
+beer_rating VARCHAR(255),
+PRIMARY KEY (beer_id)
+);
